7c975616cb511d15ad54b7f630fab2b4f73966e4
ninpnin <vainoyrjanainen@icloud.com>
2021-07-27 14:26:00 +0300

    Fork from pyriksdagen

---
+++ pyparlaclarin/export.py
@@ -0,0 +1,112 @@
+"""
+Parla Clarin generation
+"""
+import pandas as pd
+import copy
+from lxml import etree
+from pyparlaclarin.utils import infer_metadata
+
+# Generate parla clarin header
+def pc_header(metadata):
+    """
+    Generate a teiHeader for Parla-Clarin documents.
+
+    Args:
+        metadata: python dictionary with the appropriate metadata. Missing keys
+            are allowed and filled with 'N/A' values.
+    """
+    teiHeader = etree.Element("teiHeader")
+    
+    # fileDesc
+    fileDesc = etree.SubElement(teiHeader, "fileDesc")
+    
+    titleStmt = etree.SubElement(fileDesc, "titleStmt")
+    title = etree.SubElement(titleStmt, "title")
+    title.text = metadata.get("document_title", "N/A")
+    
+    if "edition" in metadata:
+        editionStmt = etree.SubElement(fileDesc, "editionStmt")
+        edition = etree.SubElement(editionStmt, "edition")
+        edition.text = metadata.get("edition", "N/A")
+
+    extent = etree.SubElement(fileDesc, "extent")
+    publicationStmt = etree.SubElement(fileDesc, "publicationStmt")
+    authority = etree.SubElement(publicationStmt, "authority")
+    authority.text = metadata.get("authority", "N/A")
+    
+    sourceDesc = etree.SubElement(fileDesc, "sourceDesc")
+    sourceBibl = etree.SubElement(sourceDesc, "bibl")
+    sourceTitle = etree.SubElement(sourceBibl, "title")
+    sourceTitle.text = metadata.get("document_title", "N/A")
+    
+    # encodingDesc
+    encodingDesc = etree.SubElement(teiHeader, "encodingDesc")
+    editorialDecl = etree.SubElement(encodingDesc, "editorialDecl")
+    correction = etree.SubElement(editorialDecl, "correction")
+    correction_p = etree.SubElement(correction, "p")
+    correction_p.text = metadata.get("correction", "No correction of source texts was performed.")
+    
+    return teiHeader
+    
+def create_parlaclarin(teis, metadata):
+    if type(teis) != list:
+        tei = teis
+        return create_parlaclarin([tei], metadata)
+    
+    teiCorpus = etree.Element("teiCorpus", xmlns="http://www.tei-c.org/ns/1.0")
+    teiHeader = pc_header(metadata)
+    teiCorpus.append(teiHeader)
+    
+    for tei in teis:
+        teiCorpus.append(tei)
+    
+    teiCorpusTree = etree.ElementTree(teiCorpus)
+    
+    for xml_element in teiCorpusTree.iter():
+        content = xml_element.xpath('normalize-space()')
+
+        if not content and len(xml_element.attrib) == 0:
+            xml_element.getparent().remove(xml_element)
+            
+    s = etree.tostring(teiCorpusTree, pretty_print=True, encoding="utf-8", xml_declaration=True).decode("utf-8")
+    return s
+    
+def create_tei(root, metadata):
+    """
+    Create a Parla-Clarin TEI element from a list of segments.
+
+    Args:
+        txts: a list of lists of strings, corresponds to content blocks and paragraphs, respectively.
+        metadata: Metadata of the parliamentary session
+    """
+    metadata = copy.deepcopy(metadata)
+    
+    tei = etree.Element("TEI")
+    protocol_id = root.attrib["id"]
+    metadata["document_title"] = protocol_id.replace("_", " ").split("-")[0].replace("prot", "Protokoll")
+    documentHeader = pc_header(metadata)
+    tei.append(documentHeader)
+    
+    text = etree.SubElement(tei, "text")
+    front = etree.SubElement(text, "front")
+    preface = etree.SubElement(front, "div", type="preface")
+    etree.SubElement(preface, "head").text = protocol_id.split(".")[0]
+    if "date" not in metadata:
+        year = metadata.get("year", 2020)
+        metadata["date"] = str(year) + "-01-01"
+        
+    etree.SubElement(preface, "docDate", when=metadata["date"]).text = metadata.get("date", "2020-01-01")
+
+    body = etree.SubElement(text, "body")
+    body_div = etree.SubElement(body, "div")
+    
+    current_speaker = None
+    current_page = 0
+    u = None
+    prev_u = None
+
+    pb = etree.SubElement(body_div, "pb")
+    pb.attrib["n"] = str(current_page)
+    
+    return tei
+
---
+++ pyparlaclarin/refine.py
@@ -0,0 +1,144 @@
+from lxml import etree
+from pyparlaclarin.utils import element_hash
+import re, random, datetime
+
+def _iter(root, ns="{http://www.tei-c.org/ns/1.0}"):
+    for body in root.findall(".//" + ns +"body"):
+        for div in body.findall(ns + "div"):
+            for ix, elem in enumerate(div):
+                if elem.tag == ns + "u":
+                    yield "u", elem
+                elif elem.tag == ns + "note":
+                    yield "note", elem
+                elif elem.tag == ns + "pb":
+                    yield "pb", elem
+                elif elem.tag == ns + "seg":
+                    yield "seg", elem
+                elif elem.tag == "u":
+                    elem.tag = ns + "u"
+                    yield "u", elem
+                else:
+                    print(elem.tag)
+                    yield None
+
+def random_classifier(paragraph):
+    alternatives = ["note", "u"]
+    return random.choice(alternatives)
+
+
+def reclassify(root, classifier, tei="{http://www.tei-c.org/ns/1.0}"):
+    """
+    Reclassify nodes in a Parla-Clarin tree.
+
+    Args:
+        root: root of the lxml tree to be reclassified
+        classifier: lambda function that classifies paragraphs. str->str,
+            takes paragraph content as input, outputs predicted xml tag, such
+            as note or u.
+        tei: namespace for the output xml
+    """
+    prev_elem = None
+    for ix, elem_tuple in enumerate(list(_iter(root))):
+        tag, elem = elem_tuple
+
+        prev_elem = elem
+        if tag == "u":
+            for seg in elem:
+                paragraph = seg.text
+                c = classifier(paragraph)
+                if c != "u":
+                    print("Change u to note")
+                    prev_elem.addnext(seg)
+                    prev_elem = seg
+                    seg.tag = tei + c
+                elif prev_elem != elem:
+                    if prev_elem.tag == tei + "u":
+                        prev_elem.append(seg)
+                    else:
+                        new_elem = etree.Element(tei + "u")
+                        prev_elem.addnext(new_elem)
+                        prev_elem = new_elem
+                        prev_elem.append(seg)
+                else:
+                    pass
+
+        elif tag == "note":
+            paragraph = elem.text
+            c = classifier(paragraph)
+            if c != tag:                
+                if c == "u":
+                    elem.tag = tei + "seg"
+                    if prev_elem.tag == tei + "u":
+                        print("Change note to u")
+                    else:
+                        # Create new u node
+                        new_elem = etree.Element(tei + c)
+                        prev_elem.addnext(new_elem)
+                        prev_elem = new_elem
+
+                    prev_elem.append(elem)
+
+                else:
+                    prev_elem = elem
+                    elem.tag = tei + c
+            else:
+                prev_elem = elem
+        else:
+            prev_elem = elem
+    return root
+
+
+def format_paragraph(paragraph, spaces=12):
+    """
+    Formats paragraphs to be equal in width.
+
+    Args:
+        paragraph: paragraph content, str.
+        spaces: size of indentation as number of spaces.
+    """
+    words = paragraph.replace("\n", "").strip().split()
+    s = "\n" + " " * spaces
+    row = ""
+
+    for word in words:
+        if len(row) > 60:
+            s += row.strip() + "\n" + " " * spaces
+            row = word
+        else:
+            row += " " + word
+
+    if len(row.strip()) > 0:
+        s += row.strip() + "\n" + " " * (spaces - 2)
+
+    if s.strip() == "":
+        return None
+    return s
+
+def format_texts(root):
+    """
+    Formats all text elements in a Parla-Clarin document.
+
+    Args:
+        root: Parla-Clarin document as an lxml tree root.
+    """
+    for tag, elem in _iter(root):
+
+        if type(elem.text) == str:
+            elem.text = format_paragraph(elem.text)
+        elif tag == "u":
+            if len(list(elem)) > 0:
+                for seg in elem:
+                    if type(seg.text) == str:
+                        seg.text = format_paragraph(seg.text, spaces=14)
+                    else:
+                        seg.text = None
+                elem.text = None
+            else:
+                elem.getparent().remove(elem)
+        elif tag == "pb":
+            if "{http://www.w3.org/XML/1998/namespace}url" in elem.attrib:
+                url = elem.attrib["{http://www.w3.org/XML/1998/namespace}url"]
+                del elem.attrib["{http://www.w3.org/XML/1998/namespace}url"]
+                elem.attrib["facs"] = url
+
+    return root
